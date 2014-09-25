8d6868511b731a6e75303ee5823889a7e5efb2e3
Jannik Hoffjann <hallo@jannikhoffjann.de>
2014-09-25 17:27:33 +0200

    good working version, very satisfied with it

--- dev/OpenCV-2.4.9-Test/src/Main.java
+++ dev/OpenCV-2.4.9-Test/src/Main.java
@@ -5,6 +5,7 @@ import org.opencv.core.*;
 import org.opencv.features2d.*;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.highgui.*;
+import org.opencv.calib3d.*;
 
 import java.util.*;
 
@@ -27,13 +28,13 @@ public class Main {
 class Demo {
 
     // Create Feature Detector
-    FeatureDetector surfFeatureDetector = FeatureDetector.create(FeatureDetector.BRISK);
+    FeatureDetector surfFeatureDetector = FeatureDetector.create(FeatureDetector.SURF);
 
     // Create Surf Extractor
-    DescriptorExtractor surfDescriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.FREAK);
+    DescriptorExtractor surfDescriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
 
     // Create Matcher
-    DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
+    DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.FLANNBASED);
 
 
     // Keypointsafes
@@ -46,7 +47,6 @@ class Demo {
 
 
     /**
-     *
      * @param image
      * @return
      */
@@ -57,7 +57,6 @@ class Demo {
 
 
     /**
-     *
      * @param logo
      */
     public void learnAboutLogo(Mat logo) {
@@ -70,20 +69,21 @@ class Demo {
 
 
     /**
-     *
      * @param matches
      * @return
      */
-    public MatOfDMatch getGoodMatches(ArrayList<MatOfDMatch> matches){
-        double max_dist = 0;
+    public MatOfDMatch getGoodMatches(ArrayList<MatOfDMatch> matches) {
+        // double max_dist = 0;
 
-        double min_dist = 1000;
+        // double min_dist = 1000;
 
-        List<DMatch> matchList = matches.get(0).toList();
+        List<DMatch> good_matches = new ArrayList<DMatch>();
+        for (int j = 0; j < matches.size(); j++) {
+            List<DMatch> matchList = matches.get(j).toList();
 
 
-        // filter for best match
-        for (int i = 0; i < logoDescriptors.rows(); i++) {
+            // filter for best match
+        /*for (int i = 0; i < matchList.size(); i++) {
 
             double dist = matchList.get(i).distance;
             if (dist < min_dist) min_dist = dist;
@@ -91,27 +91,54 @@ class Demo {
         }
 
         System.out.println(min_dist);
-        System.out.println(max_dist);
+        System.out.println(max_dist);*/
 
-        // filter matches for usable ones
+            // filter matches for usable ones
 
-        List<DMatch> good_matches = new ArrayList<DMatch>();
+            if (matchList.get(0).distance < 0.7 * matchList.get(1).distance) {
+                good_matches.add(matchList.get(0));
 
-        for(int i = 0; i < matchList.size(); i ++){
-            if(matchList.get(i).distance < 1.2 * min_dist){
-                good_matches.add(matchList.get(i));
             }
+
+
         }
-
-
         MatOfDMatch goodmatches = new MatOfDMatch();
         goodmatches.fromList(good_matches);
-
+        if (good_matches.size() < 10) {
+            System.out.println("No object found!");
+            return new MatOfDMatch();
+        }
         return goodmatches;
+
+
     }
 
-    
+    public Mat createHomography(MatOfDMatch goodmatches){
+        List<Point> srcPoints = new ArrayList<Point>();
+        List<Point> dstPoints = new ArrayList<Point>();
 
+        List<KeyPoint> logoKeypointList = logoKeypoints.get(0).toList();
+        List<KeyPoint> imgKeypointList = imageKeypoints.get(0).toList();
+
+        List<DMatch> matches = goodmatches.toList();
+        for (int i = 0; i < matches.size(); i++){
+            srcPoints.add(logoKeypointList.get(matches.get(i).queryIdx).pt);
+            dstPoints.add(imgKeypointList.get(matches.get(i).trainIdx).pt);
+        }
+
+        MatOfPoint2f matSrcPoints = new MatOfPoint2f();
+        MatOfPoint2f matDstPoints = new MatOfPoint2f();
+
+        matSrcPoints.fromList(srcPoints);
+        matDstPoints.fromList(dstPoints);
+
+        Mat mask = new Mat();
+
+        Calib3d.findHomography(matSrcPoints, matDstPoints, Calib3d.RANSAC, 5.0, mask);
+
+        return mask;
+
+    }
 
     /**
      *
@@ -122,8 +149,8 @@ class Demo {
 
         imageKeypoints.add(new MatOfKeyPoint());
         // images
-        Mat logo = Highgui.imread(getClass().getResource("/resources/fuenf-euro.jpg").getPath());
-        Mat image = Highgui.imread(getClass().getResource("/resources/cocacola2.jpg").getPath());
+        Mat logo = Highgui.imread(getClass().getResource("/resources/dhl-logo.jpg").getPath());
+        Mat image = Highgui.imread(getClass().getResource("/resources/dhl-car.jpg").getPath());
 
         // Convert RGB to Grayscale because SURF is only able to work that way
         logo = convertToGrayScale(logo);
@@ -144,22 +171,25 @@ class Demo {
         matches.add(new MatOfDMatch());
 
         // Match it
-        matcher.match(logoDescriptors, descriptors2, matches.get(0));
+        matcher.knnMatch(logoDescriptors, descriptors2, matches, 2);
 
         // filter for "good matches"
 
 
         matches.add(1, getGoodMatches(matches));
 
+        Mat mask = createHomography(matches.get(1));
+
+        
 
         Mat outImg = new Mat();
         // write images
         Features2d.drawKeypoints(logo, logoKeypoints.get(0), outImg);
-        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/fuenfer.jpg", outImg);
+        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/pic1.jpg", outImg);
         Features2d.drawKeypoints(image, imageKeypoints.get(0), outImg);
-        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/fuenfer2.jpg", outImg);
+        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/pic2.jpg", outImg);
         Features2d.drawMatches(logo, logoKeypoints.get(0), image, imageKeypoints.get(0), matches.get(1), outImg, new Scalar(0, 255, 0), new Scalar(0, 0, 255), new MatOfByte(), Features2d.NOT_DRAW_SINGLE_POINTS);
-        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/match_fuenfer.jpg", outImg);
+        Highgui.imwrite("/Volumes/HDD/Jannik/Desktop/match.jpg", outImg);
 
 
     }
